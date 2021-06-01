f92f865143f3e0a78984cd5e51f7a5bc9a02d805
HaiPC <haipc@HaiPCs-MacBook-Air.local>
2021-06-01 17:27:30 +0700

    update index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda and Github!ver2'),
+        body: JSON.stringify('Hello from Lambda and Github!ver3'),
     };
     return response;
 };
