43271b3cd40169fadc56c4bd91846580fde3f73c
HaiPC <haipc@HaiPCs-MacBook-Air.local>
2021-06-03 11:07:49 +0700

    update buildspec file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda and Github!ver5'),
+        body: JSON.stringify('Hello from Lambda and Github!ver6'),
     };
     return response;
 };
